7db63c5e9eac59df8503a4b34c705e1c94ef52a1
dmccullough <dmccullough@decisionpatterns.com>
2017-01-29 13:08:56 -0500

    remove dist from version control

--- dist/scripts/controllers/RoomsCtrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-     function RoomsCtrl($scope, $firebaseArray, Room) {
-         this.rooms = Room.all;
-     }
- 
-     angular
-         .module('blocChat')
-         .controller('RoomsCtrl', ['$scope', '$firebaseArray', 'Room', RoomsCtrl]);
- })();
