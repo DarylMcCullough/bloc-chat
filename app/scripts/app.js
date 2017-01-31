28c9a12bca11487a3ac15d9f30333f919b31d1c7
dmccullough <dmccullough@decisionpatterns.com>
2017-01-31 10:51:17 -0500

    merge new checkpoint-4 code with checkpoint-5 code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,44 @@
  (function() {
-     function config($stateProvider, $locationProvider) {
-        $locationProvider
-            .html5Mode({
-                enabled: true,
-                requireBase: false
-        });
-         
-        $stateProvider
-         .state('rooms', {
-             url: '/',
-             controller: 'RoomsCtrl as rooms',
-             templateUrl: '/templates/rooms.html'
-          })
-
- }
-     
-angular
-    .module('blocChat', ['ngAnimate', 'ngTouch', 'ui.bootstrap', 'ui.router', 'firebase'])
-    .config(config);
-})();
+ 	function config($stateProvider, $locationProvider) {
+ 		$locationProvider.html5Mode({
+ 			enabled: true,
+ 			requireBase: false
+ 		});
+ 		$stateProvider.state('rooms', {
+ 			url: '/',
+ 			controller: 'RoomsCtrl as rooms',
+ 			templateUrl: '/templates/rooms.html'
+ 		})
+ 	}
+     function getUserName($uibModal, $cookies) {
+         var username = $cookies.get('blocChatCurrentUser');
+         if (username != null && username !== "") {
+             console.log("Hey: got a username: " + username);
+             return;
+         }
+			var modalInstance = $uibModal.open({
+				ariaLabelledBy: 'modal-title',
+				ariaDescribedBy: 'modal-body',
+				templateUrl: '/templates/getUsername.html',
+				controller: function($scope, $uibModalInstance) {
+                    $scope.ok = function () {
+                        if ($scope.username != null && $scope.username !== "") {
+                            $uibModalInstance.close($scope.username);
+                        }
+                    };
+                }
+                
+            });
+			modalInstance.result.then(function(x) {
+				console.log('Modal dismissed at: ' + new Date());
+                console.log('username: ' + x);
+                if (x != null && x.length > 0) {
+                    $cookies.put('blocChatCurrentUser', x);
+                }
+			});
+		};
+ 	angular
+        .module('blocChat', ['ngAnimate', 'ngTouch', 'ui.bootstrap', 'ui.router', 'firebase', 'ngCookies'])
+        .config(config)
+        .run(['$uibModal', '$cookies', getUserName]);
+ })();
